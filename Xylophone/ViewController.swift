7bb43125126a44b978e2e57bac0eda14dfe90e95
Jan Lewandowski <iasiu@icloud.com>
2020-04-10 15:12:17 +0200

    finished

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,41 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    // player from AVFoundation that plays the sounds
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
-
     
-
+    @IBAction func keyTouchUp(_ sender: UIButton) {
+        play_sound(soundName: sender.currentTitle!)
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+    }
+    
+    func play_sound(soundName: String) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "wav") else
+        { return }
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            audioPlayer = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+            
+            guard let player = audioPlayer else { return }
+            
+            player.play()
+            
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
 }
-
